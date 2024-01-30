124e894773a0d613dcdef3943caefa6bf367e2cf
dilip98914 <dilip98914@gmail.com>
2024-01-30 21:55:37 +0530

    after 4 months I am touching this code again : lets goooooooo!!!!!!!

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://admin:AjWyiMZs5uGdvIPx@cluster0.n6ybqtu.mongodb.net/mylinks?retryWrites=true&w=majority',
+    mongoURI: 'mongodb+srv://admin:Dilip%40123@cluster0.n6ybqtu.mongodb.net/linkedinsidndnisn',
     url: 'mongodb+srv://admin:AjWyiMZs5uGdvIPx@cluster0.n6ybqtu.mongodb.net',
     db: 'mylinks',
     bucket: 'images',
--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 mongoose.connect(config.mongoURI, { useNewUrlParser: true }).then(() => {
   console.log('DB connected!');
 }).catch(err =>
-  console.log("err->>>>>>>>>>>>>>>>>>>>..")
+  console.log(err,"err->>>>>>>>>>>>>>>>>>>>..")
 );
 
 require('./middlewares/initPassport')
